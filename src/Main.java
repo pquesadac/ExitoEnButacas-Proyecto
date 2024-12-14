d551f2ea4e556183b8ee224362f0a65331f46884
Hikari Tamura <jinnsei.81@gmail.com>
2024-12-14 16:18:54 +0100

    Interfaz ProcesoCompra Versión 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,26 @@
+/*public class Main {
+    public static void main(String[] args) {
+        Sala sala = new Sala();
+        LoginGUI loginGUI = new LoginGUI();
+        loginGUI.setVisible(true);
+    }
+}*/
+
+/*public class Main {
+    public static void main(String[] args) {
+        // Crear la sala y la interfaz gráfica
+        Sala sala = new Sala();
+        ProcesoCompra procesoCompraGUI = new ProcesoCompra();
+        
+        // Hacer visible la interfaz
+        procesoCompraGUI.setVisible(true);
+        
+        // Ejecutar el test de concurrencia
+        ConcurrencyTest concurrencyTest = new ConcurrencyTest();
+        concurrencyTest.iniciarTestConcurrencia(sala, procesoCompraGUI);
+    }
+}*/
+
 public class Main {
     public static void main(String[] args) {
         // Crear la sala y el usuario principal
